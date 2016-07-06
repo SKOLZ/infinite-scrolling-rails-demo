ea68e31ab76d5301aafd497b2811f104febf0a36
Ponti Gaston <gaston.ponti@wolox.com.ar>
2016-07-06 18:16:14 -0300

    Game model

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,11 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 # encoding: utf-8
+
+
+Game.create([
+  {cover: 'http://media.ignimgs.com/media/ign/imgs/minisites/topN/rpgs/top-100-rpgs_tales-of-phantasia.jpg', name: 'Tales of Phantasia'},
+  {cover: 'http://vignette3.wikia.nocookie.net/chrono/images/1/1e/Chrono_Trigger_Artwork1.jpg/revision/latest?cb=20090311222344', name: 'Chrono Trigger'},
+  {cover: 'https://upload.wikimedia.org/wikipedia/en/b/b7/Secret_of_Mana_Box.jpg', name: 'Secret of Mana'},
+  {cover: 'http://images.nintendolife.com/games/snes/terranigma/cover_large.jpg', name: 'Terranigma'}
+])
---
+++ test/factories/games.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :game do
+    cover "MyString"
+    name "MyString"
+  end
+end
